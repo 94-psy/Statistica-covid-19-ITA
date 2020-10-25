afefd2820c1390c0413859fb1f17b1c96253139e
Alessio Bevilacqua <felicecacca@gmail.com>
2020-10-25 14:52:39 +0100

    Update STATISTICHE_COVID_ITA.m
    Controllo e riscrittura di alcune parti inerenti alla lettura di dati dai file .CSV
    Rimosse alcune variabili non più utilizzate
    Rinominate alcune variabili per essere di più facile comprensione

--- STATISTICHE_COVID_ITA.m
+++ STATISTICHE_COVID_ITA.m
@@ -12,76 +12,75 @@ if ismac
 elseif isunix
     % Code to run on Linux platform
 elseif ispc
-    csvfiles = dir('COVID-19\dati-regioni\*.csv');
+    csvfilesreg = dir('COVID-19\dati-regioni\*.csv');
 else
-    csvfiles = dir('COVID-19\dati-regioni\*.csv');
+    csvfilesreg = dir('COVID-19\dati-regioni\*.csv');
 end
 % csvfiles = dir('COVID-19\dati-regioni\*.csv');
 addpath(genpath(newFolder))
-csvfiles(end)=[];
-csvfiles(end)=[];
+csvfilesreg(end)=[];
+csvfilesreg(end)=[];
 k=0;
-morti=zeros(1,length(csvfiles));
-terapia_intensiva=zeros(1,length(csvfiles));
-terapia_intensiva_giornaliera=zeros(1,length(csvfiles));
-ricoverati_sintomatici=zeros(1,length(csvfiles));
-ricoverati_asintomatici=zeros(1,length(csvfiles));
-guariti=zeros(1,length(csvfiles));
-nuovi_attuali=zeros(1,length(csvfiles));
-attuali=zeros(1,length(csvfiles));
-tot_casi=zeros(1,length(csvfiles));
-nuovi_casi=zeros(1,length(csvfiles));
-nuovi_guariti=zeros(1,length(csvfiles));
-nuovi_morti=zeros(1,length(csvfiles));
-tamponi=zeros(1,length(csvfiles));
-tamponi_per_giorno=zeros(1,length(csvfiles));
-diff_infetti=zeros(1,length(csvfiles));
-assex=zeros(1,length(csvfiles));
-guariti_su_morti_per_giorno=zeros(1,length(csvfiles));
-guariti_su_morti=zeros(1,length(csvfiles));
-nomi=zeros(1,length(csvfiles));
+deceduti=zeros(1,length(csvfilesreg));
+terapia_intensiva=zeros(1,length(csvfilesreg));
+terapia_intensiva_giornaliera=zeros(1,length(csvfilesreg));
+ricoverati_sintomatici=zeros(1,length(csvfilesreg));
+isolamento_domiciliare=zeros(1,length(csvfilesreg));
+totale_ospedalizzati=zeros(1,length(csvfilesreg));
+guariti=zeros(1,length(csvfilesreg));
+nuovi_attuali_positivi=zeros(1,length(csvfilesreg));
+attuali_positivi=zeros(1,length(csvfilesreg));
+totale_casi=zeros(1,length(csvfilesreg));
+nuovi_casi=zeros(1,length(csvfilesreg));
+nuovi_guariti=zeros(1,length(csvfilesreg));
+nuovi_deceduti=zeros(1,length(csvfilesreg));
+tamponi=zeros(1,length(csvfilesreg));
+tamponi_per_giorno=zeros(1,length(csvfilesreg));
+differenza_infetti=zeros(1,length(csvfilesreg));
+assex=zeros(1,length(csvfilesreg));
+nomi=zeros(1,length(csvfilesreg));
 % test=zeros(1,length(csvfiles));
 
 
-for file = csvfiles'
+for file = csvfilesreg'
     k=k+1;
-    [data]=readmatrix(csvfiles(k).name,'Delimiter',',');
-    nomi(k)=str2double(csvfiles(k).name(end-11:end-4));
-    for i=1:length(data) %data(1,3)= prima riga terza colonna. 7 ricoverati con sintomi, 8 terapia intensiva, 18 TOTALE CASI, 15 deceduti,14 guariti, 12 nuovi attualmente positivi, 11 totale attualmente positivi, 19 numero tamponi fatti
-        morti(k)=morti(k)+data(i,15);
+    [data]=readmatrix(csvfilesreg(k).name,'Delimiter',',');
+    nomi(k)=str2double(csvfilesreg(k).name(end-11:end-4));
+    for i=1:length(data) %data(1,3)= prima riga terza colonna. 7 ricoverati con sintomi, 8 terapia intensiva, 9 totale ospedalizzati, 10 isolamento domiciliare, 18 TOTALE CASI, 15 deceduti,14 guariti, 12 nuovi attualmente positivi, 11 totale attualmente positivi, 19 numero tamponi fatti
+        deceduti(k)=deceduti(k)+data(i,15);
         terapia_intensiva(k)=terapia_intensiva(k)+data(i,8);
         guariti(k)=guariti(k)+data(i,14);
-        attuali(k)=attuali(k)+data(i,11);
-        nuovi_attuali(k)=nuovi_attuali(k)+data(i,12);
-        tot_casi(k)=tot_casi(k)+data(i,18);
+        attuali_positivi(k)=attuali_positivi(k)+data(i,11);
+        nuovi_attuali_positivi(k)=nuovi_attuali_positivi(k)+data(i,12);
+        totale_casi(k)=totale_casi(k)+data(i,18);
         tamponi(k)=tamponi(k)+data(i,19);
-        ricoverati_sintomatici(k)=ricoverati_sintomatici(k)+data(i,9);
+        ricoverati_sintomatici(k)=ricoverati_sintomatici(k)+data(i,7);
+        totale_ospedalizzati(k)=totale_ospedalizzati(k)+data(i,9);
+        isolamento_domiciliare(k)=isolamento_domiciliare(k)+data(i,10);
+        
     end
-    guariti_su_morti(k)=guariti(k)/morti(k);
-    ricoverati_asintomatici(k)=attuali(k)-ricoverati_sintomatici(k);
     assex(k)=k;
     
 end
 
 for l = 1:length(nuovi_casi())
     if l==1
-        nuovi_casi(l)=tot_casi(l);
+        nuovi_casi(l)=totale_casi(l);
         nuovi_guariti(l)=guariti(l);
-        nuovi_morti(l)=morti(l);
+        nuovi_deceduti(l)=deceduti(l);
         tamponi_per_giorno(l)=tamponi(l);
-        diff_infetti(l)=nuovi_casi(l);
+        differenza_infetti(l)=nuovi_casi(l);
         terapia_intensiva_giornaliera(l)=terapia_intensiva(l);
 %         test(l)=100;
     else
-        nuovi_casi(l)=tot_casi(l)-tot_casi(l-1);
+        nuovi_casi(l)=totale_casi(l)-totale_casi(l-1);
         terapia_intensiva_giornaliera(l)=terapia_intensiva(l)-terapia_intensiva(l-1);
         nuovi_guariti(l)=guariti(l)-guariti(l-1);
-        nuovi_morti(l)=morti(l)-morti(l-1);
+        nuovi_deceduti(l)=deceduti(l)-deceduti(l-1);
         tamponi_per_giorno(l)=tamponi(l)-tamponi(l-1);
-        diff_infetti(l)=nuovi_casi(l)-nuovi_casi(l-1);
+        differenza_infetti(l)=nuovi_casi(l)-nuovi_casi(l-1);
 %         test(l)=((diff_infetti(l))/nuovi_casi(l-1))*100;
     end
-    guariti_su_morti_per_giorno(l)=nuovi_guariti(l)/nuovi_morti(l);
 end
 
 %%RIASSUNTO SETTIMANALE
@@ -98,17 +97,17 @@ else
 end
 
 
-Wmorti=zeros(1,settimane_totali);
+Wdeceduti=zeros(1,settimane_totali);
 Wguariti=zeros(1,settimane_totali);
-Wattuali=zeros(1,settimane_totali);
+Wattuali_positivi=zeros(1,settimane_totali);
 Wnuovi_attuali=zeros(1,settimane_totali);
-Wtot_casi=zeros(1,settimane_totali);
+Wtotale_casi=zeros(1,settimane_totali);
 Wtamponi=zeros(1,settimane_totali);
-Wricoverati_sintomatici=zeros(1,settimane_totali);
+Wcasi_sintomatici=zeros(1,settimane_totali);
 Wterapia_intensiva=zeros(1,settimane_totali);
+Wtotale_ospedalizzati=zeros(1,settimane_totali);
+Wisolamento_domiciliare=zeros(1,settimane_totali);
 
-Wricoverati_asintomatici=zeros(1,settimane_totali);
-Wguariti_su_morti=zeros(1,settimane_totali);
 
 Wassex=zeros(1,length(settimane_totali));
 last_index=1;
@@ -117,17 +116,17 @@ ending_index=7;
 for jj=1:settimane_totali
     
     for kk=last_index:ending_index
-        Wnuovi_attuali(jj)=Wnuovi_attuali(jj)+nuovi_attuali(kk);
+        Wnuovi_attuali(jj)=Wnuovi_attuali(jj)+nuovi_attuali_positivi(kk);
     end
     Wterapia_intensiva(jj)=terapia_intensiva(kk);
-    Wmorti(jj)=morti(kk);
+    Wdeceduti(jj)=deceduti(kk);
     Wtamponi(jj)=tamponi(kk);
     Wguariti(jj)=guariti(kk);
-    Wattuali(jj)=attuali(kk);
-    Wtot_casi(jj)=tot_casi(kk);
-    Wguariti_su_morti(jj)=Wguariti(jj)/Wmorti(jj);
-    Wricoverati_sintomatici(jj)=ricoverati_sintomatici(kk);
-    Wricoverati_asintomatici(jj)=Wattuali(jj)-Wricoverati_sintomatici(jj);
+    Wattuali_positivi(jj)=attuali_positivi(kk);
+    Wtotale_casi(jj)=totale_casi(kk);
+    Wisolamento_domiciliare(jj)=isolamento_domiciliare(kk);
+    Wcasi_sintomatici(jj)=ricoverati_sintomatici(kk);
+    Wtotale_ospedalizzati(jj)=totale_ospedalizzati(kk);
     Wassex(jj)=jj;
     
     if fix(ending_index/7) == numero_di_settimane
@@ -145,51 +144,51 @@ end
 
 Wnuovi_casi=zeros(1,settimane_totali);
 Wnuovi_guariti=zeros(1,settimane_totali);
-Wnuovi_morti=zeros(1,settimane_totali);
+Wnuovi_deceduti=zeros(1,settimane_totali);
 Wtamponi_per_settimana=zeros(1,settimane_totali);
 Wdiff_infetti=zeros(1,settimane_totali);
 Wguariti_su_morti_per_giorno=zeros(1,settimane_totali);
 
 for ll = 1:length(Wnuovi_casi())
     if ll==1
-        Wnuovi_casi(ll)=Wtot_casi(ll);
+        Wnuovi_casi(ll)=Wtotale_casi(ll);
         Wnuovi_guariti(ll)=Wguariti(ll);
-        Wnuovi_morti(ll)=Wmorti(ll);
+        Wnuovi_deceduti(ll)=Wdeceduti(ll);
         Wtamponi_per_settimana(ll)=Wtamponi(ll);
         Wdiff_infetti(ll)=Wnuovi_casi(ll);
     else        
-        Wnuovi_casi(ll)=Wtot_casi(ll)-Wtot_casi(ll-1);
+        Wnuovi_casi(ll)=Wtotale_casi(ll)-Wtotale_casi(ll-1);
         Wnuovi_guariti(ll)=Wguariti(ll)-Wguariti(ll-1);
-        Wnuovi_morti(ll)=Wmorti(ll)-Wmorti(ll-1);
+        Wnuovi_deceduti(ll)=Wdeceduti(ll)-Wdeceduti(ll-1);
         Wtamponi_per_settimana(ll)=Wtamponi(ll)-Wtamponi(ll-1);
         Wdiff_infetti(ll)=Wnuovi_casi(ll)-Wnuovi_casi(ll-1);
     end
-    Wguariti_su_morti_per_giorno(ll)=Wnuovi_guariti(ll)/Wnuovi_morti(ll);
+    Wguariti_su_morti_per_giorno(ll)=Wnuovi_guariti(ll)/Wnuovi_deceduti(ll);
 end
 
 %%plotting%%
 
 figure('Name','Nuovi casi')
 subplot(2,1,1)
-plot(assex,nuovi_attuali,'r-+'), grid on
+plot(assex,nuovi_attuali_positivi,'r-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
 legend('Nuovi attualmente infetti')
 subplot(2,1,2)
-plot(assex,tot_casi,'k-+',assex,attuali,'b-+',assex,guariti,'r-+'), grid on
+plot(assex,totale_casi,'k-+',assex,attuali_positivi,'b-+',assex,guariti,'r-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
 legend('Totali','Attualmente attivi','Guariti')
 
 
-figure('Name','Morti, guariti e nuovi casi')
+figure('Name','Deceduti, guariti e nuovi casi')
 subplot(2,2,1)
-plot(assex,guariti,'r-+',assex,morti,'b-+'), grid on
+plot(assex,guariti,'r-+',assex,deceduti,'b-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
-legend('Attualmente Guariti','Totale Morti')
+legend('Attualmente Guariti','Totale Deceduti')
 subplot(2,2,2)
-plot(assex,diff_infetti,'b-+'), grid on
+plot(assex,differenza_infetti,'b-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
 legend('Differenza infetti')
@@ -199,7 +198,7 @@ ylabel('Numero casi')
 xlabel('Tempo [au]')
 legend('Nuovi infetti')
 subplot(2,2,4)
-plot(assex,nuovi_guariti,'r-+',assex,nuovi_morti,'b-+'), grid on
+plot(assex,nuovi_guariti,'r-+',assex,nuovi_deceduti,'b-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
 legend('Nuovi guariti','Nuovi morti')
@@ -218,10 +217,10 @@ ylabel('Numero tamponi')
 xlabel('Tempo [au]')
 legend('Numero tamponi per giorno')
 subplot(2,2,3)
-plot(assex,ricoverati_sintomatici,'r-+',assex,ricoverati_asintomatici,'b-+',assex,attuali,'g-+'), grid on
+plot(assex,ricoverati_sintomatici,'r-+',assex,isolamento_domiciliare,'g-+',assex,totale_ospedalizzati,'k-+'), grid on
 ylabel('Numero casi')
 xlabel('Tempo [au]')
-legend('Ricoverati sintomatici','Ricoverati asintomatici','Ricoverati totali')
+legend('Ricoverati sintomatici','Isolamento domiciliare','Totale ospedalizzati')
 subplot(2,2,4)
 plot(assex,terapia_intensiva,'r-+',assex,terapia_intensiva_giornaliera,'b-+'), grid on
 ylabel('Numero casi')
@@ -237,18 +236,18 @@ ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
 legend('Nuovi attualmente infetti')
 subplot(2,1,2)
-plot(Wassex,Wtot_casi,'k-+',Wassex,Wattuali,'b-+',Wassex,Wguariti,'r-+'), grid on
+plot(Wassex,Wtotale_casi,'k-+',Wassex,Wattuali_positivi,'b-+',Wassex,Wguariti,'r-+'), grid on
 ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
 legend('Totali','Attualmente attivi','Guariti')
 
 
-figure('Name','Morti, guariti e nuovi casi #2')
+figure('Name','Deceduti, guariti e nuovi casi #2')
 subplot(2,2,1)
-plot(Wassex,Wguariti,'r-+',Wassex,Wmorti,'b-+'), grid on
+plot(Wassex,Wguariti,'r-+',Wassex,Wdeceduti,'b-+'), grid on
 ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
-legend('Attualmente Guariti','Totale Morti')
+legend('Attualmente Guariti','Totale Deceduti')
 subplot(2,2,2)
 plot(Wassex,Wdiff_infetti,'b-+'), grid on
 ylabel('Numero casi per settimana')
@@ -260,7 +259,7 @@ ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
 legend('Nuovi infetti per settimana')
 subplot(2,2,4)
-plot(Wassex,Wnuovi_guariti,'r-+',Wassex,Wnuovi_morti,'b-+'), grid on
+plot(Wassex,Wnuovi_guariti,'r-+',Wassex,Wnuovi_deceduti,'b-+'), grid on
 ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
 legend('Nuovi guariti','Nuovi morti')
@@ -278,10 +277,10 @@ ylabel('Tamponi effettuati')
 xlabel('N° settimane passate [au]')
 legend('Numero tamponi effettuati nella settimana')
 subplot(2,2,3)
-plot(Wassex,Wricoverati_sintomatici,'r-+',Wassex,Wricoverati_asintomatici,'b-+',Wassex,Wattuali,'g-+'), grid on
+plot(Wassex,Wcasi_sintomatici,'r-+',Wassex,Wisolamento_domiciliare,'g-+',Wassex,Wtotale_ospedalizzati,'k-+'), grid on
 ylabel('Numero casi per settimana')
 xlabel('N° settimane passate [au]')
-legend('Ricoverati sintomatici','Ricoverati asintomatici','Ricoverati totali')
+legend('Ricoverati sintomatici','Isolamento domiciliare','Totale ospedalizzati')
 subplot(2,2,4)
 plot(Wassex,Wterapia_intensiva,'r-+'), grid on
 ylabel('Numero casi per settimana')
